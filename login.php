cf83b0f6c9bcdb0456e6078cb11616924d248557
tomrogers123 <tom@therogersfamilyonline.com>
2012-05-04 22:04:40 +0100

    Added foundation for user logout functionality.

--- login.php
+++ login.php
@@ -2,7 +2,12 @@
   
   session_start();
 
-  if (isset$_POST['submit']) {
+  if (isset($_GET['logout']) && isset($_SESSION['CORE_LOGGED_IN'])) {
+  	session_destroy();
+  	$_GET['e'] = 3;
+  }
+
+  if (isset($_POST['submit'])) {
   	if (!empty($_POST['username']) && !empty($_POST['password']) ) {
   		$username = mysql_real_escape_string($_POST['username']);
   		$password = md5(mysql_real_escape_string($_POST['password']));
@@ -14,8 +19,9 @@
   			$_SESSION['CORE_LOGGED_IN'] = true;
   			header("Location: index.php");
   		}
+
   	}else{
-  		header("Location: login.php?e=2")
+  		header("Location: login.php?e=2");
   	}
 
   	if($_POST['remember'] == 'on') {
@@ -54,6 +60,10 @@
 			  	echo "<p>Those details were not found</p>";	
 			  break;
 
+			  case 3:
+			  	echo "<p>You have been successfully logged out.</p>";
+			  break;
+
 			}
 			echo "</div>";
 		} ?>
@@ -62,12 +72,12 @@
 			
 			<div>
 				<label for="username">Username:</label>
-				<input type="text" id="username" id="username" value="<?php echo $_COOKIE['CStudies_usr'] ?>">
+				<input type="text" id="username" id="username" value="<?php $_COOKIE['CStudies_usr'] ?>">
 			</div>
 
 			<div>
 				<label for="password">Password:</label>
-				<input type="text" id="username" id="username" value="<?php echo $_COOKIE['CStudies_pass'] ?>">
+				<input type="password" id="password" id="password" value="<?php $_COOKIE['CStudies_pass'] ?>">
 			</div>
 
 			<div>
